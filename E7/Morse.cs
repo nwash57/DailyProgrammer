8dbcfdffe8e78ba678648682482a34cfe46d7d41
Nathan Wash <nwash57@gmail.com>
2014-11-14 14:23:06 -0600

    11-14-14 2:22

--- E7/Morse.cs
+++ E7/Morse.cs
@@ -17,22 +17,197 @@ namespace E7
             switch (input)
             {
                 case 1:
-                    ToMorse();
+                    Console.WriteLine(ToMorse());
+                    Again();
                     break;
                 case 2:
                     ToAlphabetic();
+                    Again();
                     break;
             }
         }
 
-        public static void ToMorse()
+        public static string ToMorse()
         {
+            string input;
+            char[] ca;
+            StringBuilder sb = new StringBuilder();
 
+            Console.WriteLine("Input the string you would like converted to Morse Code.");
+            input = Console.ReadLine();
+            ca = input.ToCharArray();
+
+            for (int i = 0; i < ca.Length; i++)
+            {
+                sb.Append(ConvertChar(ca[i]));
+            }
+            return sb.ToString();
         }
 
         public static void ToAlphabetic()
         {
 
+        }
+
+        public static string ConvertChar(char c)
+        {
+            string output = "";
+
+            switch (c)
+            {
+                case 'a':
+                case 'A':
+                    output = ".- ";
+                    break;
+                case 'b':
+                case 'B':
+                    output = "-... ";
+                    break;
+                case 'c':
+                case 'C':
+                    output = "-.-. ";
+                    break;
+                case 'd':
+                case 'D':
+                    output = "-.. ";
+                    break;
+                case 'e':
+                case 'E':
+                    output = ". ";
+                    break;
+                case 'f':
+                case 'F':
+                    output = "..-. ";
+                    break;
+                case 'g':
+                case 'G':
+                    output = "--. ";
+                    break;
+                case 'h':
+                case 'H':
+                    output = ".... ";
+                    break;
+                case 'i':
+                case 'I':
+                    output = ".. ";
+                    break;
+                case 'j':
+                case 'J':
+                    output = ".--- ";
+                    break;
+                case 'k':
+                case 'K':
+                    output = "-.- ";
+                    break;
+                case 'l':
+                case 'L':
+                    output = ".-.. ";
+                    break;
+                case 'm':
+                case 'M':
+                    output = "-- ";
+                    break;
+                case 'n':
+                case 'N':
+                    output = "-. ";
+                    break;
+                case 'o':
+                case 'O':
+                    output = "--- ";
+                    break;
+                case 'p':
+                case 'P':
+                    output = ".--. ";
+                    break;
+                case 'q':
+                case 'Q':
+                    output = "--.- ";
+                    break;
+                case 'r':
+                case 'R':
+                    output = ".-. ";
+                    break;
+                case 's':
+                case 'S':
+                    output = "... ";
+                    break;
+                case 't':
+                case 'T':
+                    output = "- ";
+                    break;
+                case 'u':
+                case 'U':
+                    output = "..- ";
+                    break;
+                case 'v':
+                case 'V':
+                    output = "...- ";
+                    break;
+                case 'w':
+                case 'W':
+                    output = ".-- ";
+                    break;
+                case 'x':
+                case 'X':
+                    output = "-..- ";
+                    break;
+                case 'y':
+                case 'Y':
+                    output = "-.-- ";
+                    break;
+                case 'z':
+                case 'Z':
+                    output = "--.. ";
+                    break;
+                case '1':
+                    output = ".---- ";
+                    break;
+                case '2':
+                    output = "..--- ";
+                    break;
+                case '3':
+                    output = "...-- ";
+                    break;
+                case '4':
+                    output = "....- ";
+                    break;
+                case '5':
+                    output = "..... ";
+                    break;
+                case '6':
+                    output = "-.... ";
+                    break;
+                case '7':
+                    output = "--... ";
+                    break;
+                case '8':
+                    output = "---.. ";
+                    break;
+                case '9':
+                    output = "----. ";
+                    break;
+                case '0':
+                    output = "----- ";
+                    break;
+                case ' ':
+                    output = "/ ";
+                    break;
+            }
+            return output;
+        }
+
+        public static void Again()
+        {
+            string input;
+
+            Console.WriteLine("\nRun the program again? y/n");
+            input = Console.ReadLine();
+
+            if (input.Equals("y"))
+            {
+                Main(null);
+            }
+            else System.Environment.Exit(1);
         }
     }
 }
