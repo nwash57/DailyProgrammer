597775ebb358238e4d6eaf476952591467705186
Nathan Wash <nwash57@gmail.com>
2014-11-16 01:20:15 -0600

    11-16-2014 1:20AM

---
+++ E010/TeleNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace E010
+{
+    class TeleNum
+    {
+        public static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"\d{3}[.-]\d{3}[.-]\d{4}|[(]\d{3}[)]\s\d{3}.\d{4}|[(]\d{3}[)]\d{3}[-.]\d{4}|\d{10}|\d{7}");
+            string num;
+
+            Console.WriteLine("\nInsert a valid telephone number.");
+            num = Console.ReadLine();
+
+            if (regex.Match(num).Success)
+                Console.WriteLine("Valid");
+            else Console.WriteLine("Invalid");
+            Main(null);
+        }
+    }
+}
---
+++ E011/DateToDayOfWeek.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E011
+{
+    class DateToDayOfWeek
+    {
+        public static void Main(string[] args)
+        {
+            int Y, y, c, d, m, w;
+            string dayOfWeek = "";
+
+            Console.WriteLine("\nDay: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Month: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Year: ");
+            Y = Convert.ToInt32(Console.ReadLine());
+
+            if (m < 3)
+            {
+                m += 12;
+                Y -= 1;
+            }
+
+            y = Y;
+            while (y > 99)
+            {
+                y -= 100;
+            }
+
+            c = Y;
+            while (!(c % 100 == 0))
+            {
+                c--;
+            }
+            c /= 100;
+
+            w = (d + ((13 * (m + 1)) / 5) + y + (y / 4) + (c / 4) - (2 * c)) % 7;
+
+            switch (w)
+            {
+                case 2:
+                    dayOfWeek = "Monday";
+                    break;
+                case 3:
+                    dayOfWeek = "Tuesday";
+                    break;
+                case 4:
+                    dayOfWeek = "Wednesday";
+                    break;
+                case 5:
+                    dayOfWeek = "Thursday";
+                    break;
+                case 6:
+                    dayOfWeek = "Friday";
+                    break;
+                case 0:
+                    dayOfWeek = "Saturday";
+                    break;
+                case 1:
+                    dayOfWeek = "Sunday";
+                    break;
+            }
+            Console.WriteLine(dayOfWeek);
+
+            Main(null);
+        }
+    }
+}
