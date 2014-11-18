ffe09ddd42478dc5f753007200554f3f107d027b
Nathan Wash <nwash57@gmail.com>
2014-11-18 14:24:13 -0600

    11-18-14 2:23PM

---
+++ E014/ReverseStuff.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E014
+{
+    class ReverseStuff
+    {
+        public static List<string> list = new List<string>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Input strings and press enter. Input 'done' to end input.");
+            Console.Write("Input: ");
+
+            string input = Console.ReadLine();
+            while (!input.Equals("done"))
+            {
+                Console.Write("Input: ");
+                input = Console.ReadLine();
+                list.Add(input);
+            }
+
+            Console.WriteLine("How large of blocks would you like to swap?");
+            Console.Write("Block Size: ");
+
+            int blockSize = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (((i * blockSize) % blockSize) % 1 == 0 && (i + blockSize < list.Count))
+                {
+                    list.Reverse(i, blockSize);
+                }
+            }
+
+            for (int i = 0; i < list.Count; i++)
+                Console.WriteLine(list[i].ToString());
+            Console.Read();
+        }
+    }
+}
